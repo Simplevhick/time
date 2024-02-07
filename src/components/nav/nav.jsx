342a9176db51d9df3bb79f9888a751d10578717b
simplevhick <chimborvictor10@gmail.comgit config --global user.email chimborvictor10@gmail.com>
2024-02-07 15:43:48 +0100

    adjusted the div

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -140,7 +140,7 @@ const Nav = () => {
           backgroundImage: `url(${Img})`,
         }}
       >
-        <div className="lg:h-[25rem] lg:w-[31rem] lg:bg-black lg:bg-opacity-20 lg:border-none lg:border-2 lg:absolute lg:top-[39rem] lg:left-[2rem] sm:h-[11.9rem] sm:w-[12.4rem] sm:bg-black sm:bg-opacity-15 sm-border-none sm:absolute sm:top-[10rem] sm:left-[0.35rem] md:h-[17.5rem] md:w-[16.5rem] md:bg-black md:bg-opacity-20 md:border-none md:absolute md:top-[18rem] md:left-[1.6rem]">
+        <div className="lg:h-[25rem] lg:w-[31rem] lg:bg-black lg:bg-opacity-20 lg:border-none lg:border-2 lg:absolute lg:top-[39rem] lg:left-[2rem] sm:h-[11.7rem] sm:w-[12.4rem] sm:bg-black sm:bg-opacity-15 sm-border-none sm:absolute sm:top-[10rem] sm:left-[0.35rem] md:h-[17.5rem] md:w-[16.5rem] md:bg-black md:bg-opacity-20 md:border-none md:absolute md:top-[18rem] md:left-[1.6rem]">
           <div>
             <h1 className="lg:text-6xl lg:ml-[21px] lg:mt-[25px] Poppins lg:text-[#00A3BA] sm:text-[22px] sm:mt-[5px] sm:ml-[14px] sm:leading-[20px] sm:text-[#00A3BA] md:text-[28px] md:leading-[29px] md:ml-[21px] md:mt-[19px] md:text-[#00A3BA]">
               We redefine entertainment.
